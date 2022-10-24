f1778bab3f18a49a0d848494e2a9df6328f73ce7
Blake Strawhorn <strawhob@usca.edu>
2022-10-24 13:14:04 -0400

    october 24

---
+++ js/quiz.js
@@ -0,0 +1,13 @@
+function changestyle(){
+    var classname=this.getAttribute("class","answer");
+    if(classname=="answer"){
+    this.setAttribute("class","answer selected");
+    }
+    else{
+        this.setAttribute("class","answer");
+    }
+
+}
+for(var i=0;i<4;i++){
+document.getElementsByClassName('answer')[i].addEventListener('change',changestyle,false);
+}
